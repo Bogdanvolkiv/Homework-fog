81b297368f140047c2e19de0f5b9e52a73935714
unknown <bogdan.volk0807@gmail.com>
2024-05-03 15:43:18 +0300

    Вывод массива на экран

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿string [] array = new string[] { "top", " my", "pi", "cook", "tip" };
 
-for (int i = 0; i < array.Length; i++)
+for (int i = 0; i < array.Length-1; i++)
 {
-    
+    Console.WriteLine($"{array[i]} ");
 }
