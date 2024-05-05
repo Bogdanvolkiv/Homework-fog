53bcc53e59763e892da03b3c6cf4c1ce1debad31
unknown <bogdan.volk0807@gmail.com>
2024-05-05 23:58:18 +0300

    Создание условий и циклов

--- Program.cs
+++ Program.cs
@@ -1,1 +1,11 @@
 ﻿string [] array = new string[] { "top", " my", "pi", "cook", "tip", "Hello", "content" };
+int maxLenght = 3;
+
+int newSize = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if(array[i].Length <= maxLenght)
+    {
+        newSize++;
+    }
+}
